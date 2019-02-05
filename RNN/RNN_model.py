e7cfa5f646212b57bfc64e97832a964f03e0b200
Yaroslav Harmatyuk <42894823+Yarharm@users.noreply.github.com>
2019-02-04 20:54:25 -0500

    Start building RNN model
    Implement constructor and finish embedded layer

--- RNN/RNN_model.py
+++ RNN/RNN_model.py
@@ -30,7 +30,7 @@ for review in df['review']:
 # Adjust review to have the same size
 # Left-padding (Length = 200 => open to Optimization)
 #print(np.array(mapped_reviews).shape)
-padded_sequence_len = 256
+padded_sequence_len = 200
 sequences = np.zeros((df.shape[0], padded_sequence_len), dtype=int)  # zero matrix
 
 for i, review in enumerate(mapped_reviews):
@@ -68,3 +68,45 @@ def batch_generator(X, y=None, batch_size=64):
 ## Word sequence must be converted to the input features
 ## One Hot Enconding (Bad solution: Matrix is too sparse)
 ## Embedding (Great solution)
+
+# RNN model
+class RNN(object):
+    def __init__(self, n_words, seq_len=200, lstm_size=256, num_layers=1,
+                 batch_size=64, learning_rate=0.0001, embed_size=200):
+        ## Sequence length and Embedding size are identical in this implementation
+        self.n_words = n_words  # numb of unique words
+        self.seq_len = seq_len  # length of the transformed review(Left-padded)
+        self.lstm_size = lstm_size  # numb of units in a hidden layer
+        self.num_layers = num_layers  # numb of RNN layers
+        self.batch_size = batch_size
+        self.learning_rate = learning_rate
+        self.embed_size = embed_size  # embedding layers (number of features)
+
+        #self.g = tf.Graph()
+        #with self.g.as_default():
+         #   tf.set_random_seed(1)
+         #   self.build()
+         #   self.saver = tf.train.Saver()  # Saving is optional
+         #   self.init_op = tf.global_variables_initializer()
+
+    def build(self):
+        """
+        Declares three placeholdres: input data, labels and probability of drop out layer
+        Create embedding layer and build RNN using embedded feature representation
+        :return:
+        """
+        # Placeholders
+        tf_x = tf.placeholder(tf.int32, shape=(self.batch_size, self.seq_len),
+                              name='tf_x')
+        tf_y = tf.placeholder(tf.float32, shape=(self.batch_size), name='tf_y')
+        tf_keep_proba = tf.placeholder(tf.float32, name='tf_keep_proba')
+
+        # Embedding layer (Integer encoded review => embedding feature vector)
+        embed_lay = tf.Variable(tf.random_uniform((self.n_words, self.embed_size),  # Real numbers [-1,1]
+                                                  minval=-1, maxval=1), name='embed_lay')
+        embed_x = tf.nn.embedding_lookup(embed_lay, tf_x, name='embed_x')  #look up for embedding matrix
+
+        ### Problem of Vanishing GD in RNNs
+        ## !!!!!!!!!!One of these has to be added!!!!!!!!!!
+        # Solution 1: LSTM unit
+        # Solution 2: GRU (Gated Recurrent Unit)
